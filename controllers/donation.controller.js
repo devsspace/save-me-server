a3906b3c2556cc3029873a75063bb525f461bbcd
Abdify <abdullah.fahimm08@gmail.com>
2021-08-08 22:11:22 +0600

    donations paginated

--- controllers/donation.controller.js
+++ controllers/donation.controller.js
@@ -20,12 +20,15 @@ export const getDonations = async (req, res) => {
     const { userId, role } = req;
 
     try {
+        const limit = parseInt(req.query.limit);
+        const skip = parseInt(req.query.skip);
         const data =
-            role === "admin"
-                ? await Donation.find({})
-                : await Donation.find({ "askedBy._id": userId });
-        console.log(data);
-        res.status(200).json(data);
+        role === "admin"
+        ? await Donation.find({}).sort({date: 1}).skip(skip).limit(limit)
+        : await Donation.find({ "askedBy._id": userId }).sort({date: 1}).skip(skip).limit(limit);
+        // console.log(data);
+        const total = await Donation.countDocuments({});
+        res.status(200).json({donors: data, total});
     } catch (error) {
         console.log(error);
         res.json({ message: error.message || "something went wrong" });
