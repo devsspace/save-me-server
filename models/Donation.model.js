e8de54184617f6ef8434c6acad417d35d37e0f20
Abdify <abdullah.fahimm08@gmail.com>
2021-08-08 09:28:54 +0600

    donation model updated - to prevent bad requests

--- models/Donation.model.js
+++ models/Donation.model.js
@@ -2,22 +2,28 @@ import mongoose from "mongoose";
 const { model, Schema } = mongoose;
 
 const donationSchema = new Schema({
-  askedBy: {
-    type: Schema.Types.Mixed,
-    required: true,
-  },
-  askedTo: {
-    type: Schema.Types.Mixed,
-    required: true,
-  },
-  date: {
-    type: Date,
-    require: true,
-  },
-  details: {
-    type: String,
-    required: true,
-  },
+    askedBy: {
+        _id: { type: String, required: true },
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        phoneNumber: { type: String, required: true },
+    },
+    askedTo: {
+        _id: { type: String, required: true },
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        bloodGroup: { type: String, required: true },
+        location: { type: String, required: true },
+        phoneNumber: { type: String, required: true },
+    },
+    date: {
+        type: Date,
+        require: true,
+    },
+    details: {
+        type: String,
+        required: true,
+    },
 });
 
 export default model("Donation", donationSchema);
